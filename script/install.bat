38f52861b948577365fa5e6c42babb94e0ef0696
gongym <1967988842@qq.com>
2023-10-23 09:28:16 +0800

    update

--- script/install.bat
+++ script/install.bat
@@ -1,4 +1,4 @@
-go get -u github.com/ggymm/gopkg
-go get -u github.com/ggymm/gopkg-auth
-go get -u github.com/ggymm/gopkg-gorm
+go get -u github.com/ggymm/gopkg@v1.1.3
+go get -u github.com/ggymm/gopkg-auth@v1.0.4
+go get -u github.com/ggymm/gopkg-gorm@v1.0.5
 go get -u github.com/ggymm/gopkg-fiber
