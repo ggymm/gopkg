f7c9a03b28b7cf920e72d6ae952a1d3f03eae6b0
gongym <1967988842@qq.com>
2024-12-26 10:40:03 +0800

    update: uuid

--- uuid/version1.go
+++ uuid/version1.go
@@ -4,6 +4,10 @@
 
 package uuid
 
+import (
+	"encoding/binary"
+)
+
 // NewUUID returns a Version 1 UUID based on the current NodeID and clock
 // sequence, and the current time.  If the NodeID has not been set by SetNodeID
 // or SetNodeInterface then it will be set automatically.  If the NodeID cannot
@@ -12,29 +16,29 @@ package uuid
 // return the current NewUUID returns nil and an error.
 //
 // In most cases, New should be used.
-//func NewUUID() (UUID, error) {
-//	var uuid UUID
-//	now, seq, err := GetTime()
-//	if err != nil {
-//		return uuid, err
-//	}
-//
-//	timeLow := uint32(now & 0xffffffff)
-//	timeMid := uint16((now >> 32) & 0xffff)
-//	timeHi := uint16((now >> 48) & 0x0fff)
-//	timeHi |= 0x1000 // Version 1
-//
-//	binary.BigEndian.PutUint32(uuid[0:], timeLow)
-//	binary.BigEndian.PutUint16(uuid[4:], timeMid)
-//	binary.BigEndian.PutUint16(uuid[6:], timeHi)
-//	binary.BigEndian.PutUint16(uuid[8:], seq)
-//
-//	nodeMu.Lock()
-//	if nodeID == zeroID {
-//		setNodeInterface("")
-//	}
-//	copy(uuid[10:], nodeID[:])
-//	nodeMu.Unlock()
-//
-//	return uuid, nil
-//}
+func _NewUUID() (UUID, error) {
+	var uuid UUID
+	now, seq, err := GetTime()
+	if err != nil {
+		return uuid, err
+	}
+
+	timeLow := uint32(now & 0xffffffff)
+	timeMid := uint16((now >> 32) & 0xffff)
+	timeHi := uint16((now >> 48) & 0x0fff)
+	timeHi |= 0x1000 // Version 1
+
+	binary.BigEndian.PutUint32(uuid[0:], timeLow)
+	binary.BigEndian.PutUint16(uuid[4:], timeMid)
+	binary.BigEndian.PutUint16(uuid[6:], timeHi)
+	binary.BigEndian.PutUint16(uuid[8:], seq)
+
+	nodeMu.Lock()
+	if nodeID == zeroID {
+		setNodeInterface("")
+	}
+	copy(uuid[10:], nodeID[:])
+	nodeMu.Unlock()
+
+	return uuid, nil
+}
